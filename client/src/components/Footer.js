2d6ccf2232d9379e08d5c83ac7b11d4e2562bb67
aldoibuado <aldo.ibuado@gmail.com>
2023-03-02 19:30:54 -0800

    added instagram link, styling for navbar

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import EmailIcon from '@mui/icons-material/Email';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import '../styles/Footer.css';
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='mediaLinks'>
+        <EmailIcon onClick={event => window.location.href='mailto:'} />
+        <InstagramIcon onClick={event => window.location.href='https://instagram.com/_theredplace_?igshid=YmMyMTA2M2Y='} />
+      </div>
+      <p> &copy; 2023 All Rights Reserved</p>
+    </div>
   )
 }
 
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import '../styles/About.css';
 
 function About() {
   return (
-    <div>About</div>
+    <div className='about'>
+      <p className='aboutus'> The Red Place is a resturant that offers a variety of different food and drinks!</p>
+    </div>
   )
 }
 
