1f4c7b5ea3ec95633aab35922a51626b52bb07ba
jhlynn95 <109389786+jhlynn95@users.noreply.github.com>
2023-03-06 11:24:14 -0800

    queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,38 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id``
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          quantity
+          image
+        }
+      }
+    }
+  }
+`;
