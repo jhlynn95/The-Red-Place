377db74d1a0d519e9de26c684d2294cb5ac8d5c4
aldoibuado <aldo.ibuado@gmail.com>
2023-03-08 21:49:25 -0800

    added tiktok logo link along with making the phone icon work

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import EmailIcon from '@mui/icons-material/Email';
+import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 import InstagramIcon from '@mui/icons-material/Instagram';
+// import AudiotrackIcon from '@mui/icons-material/Audiotrack';
+import tiktok from '../imgs/tiktok1-transformed.png';
 import '../styles/Footer.css';
 
 function Footer() {
   return (
     <div className='footer'>
       <div className='mediaLinks'>
-        <EmailIcon className='email' onClick={event => window.location.href='mailto:'} />
+        <a href='tel:9515507141'><PhoneIphoneIcon className='phone'/></a> 
         <InstagramIcon className='insta' onClick={event => window.location.href='https://instagram.com/_theredplace_?igshid=YmMyMTA2M2Y='} />
+        <img className='tiktok' src={tiktok} alt='tiktok' onClick={event => window.location.href='https://www.tiktok.com/@theredplace_mangoneadas?_t=8aUayR8wpfX&_r=1'} />
       </div>
       <p> &copy; 2023 All Rights Reserved</p>
     </div>
